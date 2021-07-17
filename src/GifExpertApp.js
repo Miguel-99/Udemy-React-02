894e31dc34c7643ea527f384515928a61a2cdd7c
miguel_condori <condori.miguel99@hotmail.com>
2021-07-17 19:15:08 -0300

    agrego componente que carga gifs

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 
 const GifExpertApp = () => {
 
 
-    const [categories, setcategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+    const [categories, setcategories] = useState(['One Punch']);
 
 //   const handleAdd = () => setcategories( cats => [...cats, 'Categoria random']);
     
@@ -14,8 +15,15 @@ const GifExpertApp = () => {
         <h2>GifExpertApp</h2>
         <AddCategory setCategories={ setcategories }/>
         <hr />
+        
         <ol>
-        {categories.map( item => <li key={item}>{item}</li>)}
+            {
+                categories.map( item => 
+                    <GifGrid 
+                        key={ item }
+                        category={ item } />     
+                )
+            }
         </ol>
     </>)
 };
--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,25 @@
 import React from 'react'
 
 export const GifGrid = ({ category }) => {
+
+    const getGifs = async() => {
+        const url = 'https://api.giphy.com/v1/gifs/search?q=anime&limit=10&api_key=LsbydioK5hy3ONtkKCP7djtQeDq8BlJ8';
+        const resp = await fetch( url );
+        const { data } = await resp.json();
+
+
+        const gifs = data.map( img => {
+            return {
+                id: img.id,
+                title: img.title,
+                url: img.images?.downsized_medium.url
+            }
+        })
+        console.log( gifs );
+    }
+
+    getGifs();
+
     return (
         <>
             <h3> {category}</h3>  
