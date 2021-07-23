c2029a2a2ebe2030f3ad6c64dc15be13b7a491a6
miguel_condori <condori.miguel99@hotmail.com>
2021-07-22 21:24:32 -0300

    limpiando codigo innecesario y utilizado por otros scripts

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,20 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { getGifs } from '../helpers/getGifs';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
-    const [images, setImages] = useState([]);
-
-    useEffect(() => {
-        getGifs( category ).then( setImages );
-    }, [ category ]);
+    const { data:images, loading}= useFetchGifs( category );
 
     return (
         <>
             <h3> {category}</h3>  
             <div className="card-grid">
-
+                { loading && <p>Cargando...</p>}
                 {
                     images.map( obj => (
                         <GifGridItem 
